917fde739c6b8ae9ccbe74ee244195583c07191a
Haji Mohammed <hajisky07@gmail.com>
2021-03-16 08:41:04 +0000

    created fake user data

---
+++ server/src/db/postData.ts
@@ -0,0 +1,16 @@
+export const posts = [
+  {
+    id: "post1",
+    name: "A nice Post1",
+    description: "This is Post 1",
+    imageUrl: "path/to/image",
+    userId: "user1",
+  },
+  {
+    id: "post2",
+    name: "Nice Post 2",
+    description: "This is Post 1",
+    imageUrl: "path/to/image",
+    userId: "user2",
+  },
+]
---
+++ server/src/db/userData.ts
@@ -0,0 +1,37 @@
+export const users = [
+  {
+    id: "user1",
+    firstName: "Kamal",
+    lastName: "Umar",
+    email: "kamal@outlook.come",
+    isVerified: true,
+  },
+  {
+    id: "user2",
+    firstName: "Sadeeq",
+    lastName: "Muhammed",
+    email: "sadeeq@outlook.com",
+    isVerified: false,
+  },
+  {
+    id: "user3",
+    firstName: "Sadeeq",
+    lastName: "Muhammed",
+    email: "sadeeq@outlook.com",
+    isVerified: false,
+  },
+  {
+    id: "user4",
+    firstName: "Sadeeq",
+    lastName: "Muhammed",
+    email: "sadeeq@outlook.com",
+    isVerified: false,
+  },
+  {
+    id: "user5",
+    firstName: "Sadeeq",
+    lastName: "Muhammed",
+    email: "sadeeq@outlook.com",
+    isVerified: false,
+  },
+]
