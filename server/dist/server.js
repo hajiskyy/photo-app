acc72aef00f2fbec88a890eb4d89b8ce4db1c855
Haji Mohammed <32811326+hajiskyy@users.noreply.github.com>
2021-03-16 08:47:41 +0000

    Delete server/dist directory

--- server/dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const apollo_server_1 = require("apollo-server");
-const schema_1 = require("./schema");
-const server = new apollo_server_1.ApolloServer(schema_1.schema);
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
