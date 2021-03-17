1ee94cd1e893730e630ea46c23ce0aa1d866ef3e
Haji Mohammed <hajisky07@gmail.com>
2021-03-17 18:42:23 +0000

    removed all server files

--- server/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloServer, gql } from "apollo-server"
-import { schema } from "./schema"
-
-const server = new ApolloServer(schema)
-
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
-})
